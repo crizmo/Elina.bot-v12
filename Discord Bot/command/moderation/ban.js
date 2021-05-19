f3aaa353f96950f53aabe23f64bddbc8b1223513
Criz <83665497+Criz-595@users.noreply.github.com>
2021-05-19 14:47:47 +0530

    Add files via upload

--- Discord Bot/command/moderation/ban.js
+++ Discord Bot/command/moderation/ban.js
@@ -1,1 +1,15 @@
-
+module.exports = {
+    name: 'ban',
+    permissions: ["ADMINISTRATOR"],
+    description: "description of ban command!",
+    execute(client, message, args){
+        const member = message.mentions.users.first();
+        if(member){
+            const memberTarget = message.guild.members.cache.get(member.id);
+            memberTarget.ban();
+            message.channel.send("User has been banned");
+        }else{
+            message.channel.send('You couldnt ban the member');
+        }
+    }
+}
