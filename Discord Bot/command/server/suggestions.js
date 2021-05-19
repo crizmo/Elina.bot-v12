2ec4ea0bb4e1fba028bf05ffcac49139d28abb35
Criz <83665497+Criz-595@users.noreply.github.com>
2021-05-19 14:49:57 +0530

    Add files via upload

---
+++ Discord Bot/command/server/suggestions.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'suggestions',
+    permissions: ["SEND_MESSAGES"],
+    aliases: ['suggest'],
+    cooldown: 5,
+    description: 'creates a suggestion!',
+    execute(client, message, args, Discord, cmd){
+        const channel = message.guild.channels.cache.find(c => c.name === 'suggestions');
+        if(!channel) return message.channel.send('suggestions channel does not exist!');
+
+        let messageArgs = args.join(' ');
+        const embed = new Discord.MessageEmbed()
+        .setColor('FADF2E')
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+        .setDescription(messageArgs);
+
+        channel.send(embed).then((msg) =>{
+            msg.react('👍');
+            msg.react('👎');
+            message.delete();
+        }).catch((err)=>{
+            throw err;
+        });
+    }
+}
