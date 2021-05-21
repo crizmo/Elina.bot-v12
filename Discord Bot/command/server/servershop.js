722d56ee2501e64888cb7339f3fce125ca6e1c39
Criz <83665497+Criz-595@users.noreply.github.com>
2021-05-21 09:44:51 +0530

    Add files via upload

---
+++ Discord Bot/command/server/servershop.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'servershop',
+    permissions: ["ADMINISTRATOR"],
+    description: "Embeds!",
+    execute(client, message, args, Discord){
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#00FFFF')
+        .setTitle('-------------------Server shop-------------------')
+        .setURL('https://discord.gg/Ecy6WpEZsD')
+        .setDescription('DrunkBetch Server Shop')
+        .addFields(
+            {name: '1st item- owo & dank', value: "Custom message by bot for 3 days -{50,000 owo coins & 5,000 Dank cash}"},
+            {name: '2nd item- owo & dank', value: "Custom role for a week - {100,000 owo coins & 10,000 Dank Cash} "},
+            {name: '3rd item- owo & dank', value: "One of your Emotes { 50,000 owo coins & 5,000 Dank Cash} "},
+            {name: '4th item- owo & dank', value: "Custom reaction on a word for a week {50,000 OwO & 5,000 Dank cash}"},
+
+        )
+        .setThumbnail("https://cdn.discordapp.com/icons/782646778347388959/a_1dc3d36a0f29ae80c960dc568588c835.gif?size=128")
+        .setImage('https://media.discordapp.net/attachments/782649549763379210/815912557708574730/server_info.gif')
+        .setFooter('Ty , If you want to buy anything just dm me to Criz');
+
+        message.channel.send(newEmbed);
+    }
+}
