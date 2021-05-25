2701232af288a1ed89937998bf0d1290cfb68a57
Criz <83665497+Criz-595@users.noreply.github.com>
2021-05-25 10:02:58 +0530

    Add files via upload

---
+++ Discord Bot/command/fun/snakecord.js
@@ -0,0 +1,24 @@
+const SnakeGame = require('snakecord');
+
+const snakeGame = new SnakeGame({
+    backg: "🟦", 
+    snake: "🟠", 
+    snakeTail: "🟡", 
+    apple: "🍩",
+    title: 'Snake Game',
+    color: "#0FFFFF",
+    timestamp: false,
+    gameOverTitle: "Game Over"
+});
+
+module.exports = {
+    name: "snakecord",
+    cooldown: 2,
+    permissions: ["SEND_MESSAGES"],
+    description: "snake game!",
+    execute(client, message, args, Discord, cmd){
+    
+	return snakeGame.newGame(message);
+    }
+}
+
