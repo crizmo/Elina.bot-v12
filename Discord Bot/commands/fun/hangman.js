67d1281aeb0a960d86c55e0029e025e9b9ca84b5
Criz <83665497+Criz-595@users.noreply.github.com>
2021-05-28 10:08:05 +0530

    Add files via upload

---
+++ Discord Bot/commands/fun/hangman.js
@@ -0,0 +1,25 @@
+  
+const { hangman } = require('reconlx')
+
+module.exports = {
+    name : 'hangman',
+    permissions: ["SEND_MESSAGES"],
+    description: 'hangman command',
+    execute (client, message, args, Discord) {
+        if(!message.member.hasPermission("SEND_MESSAGES")) return message.channel.send('You need manage messages permission.')
+        const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
+        if(!channel) return message.channel.send('Please specify a channel')
+        const word = args.slice(1).join(" ")
+        if(!word) return  message.channel.send('Please specify a word to guess.')
+
+        const hang = new hangman({
+            message: message,
+            word: word,
+            client: client,
+            channelID: channel.id,
+        })
+
+        hang.start();
+        message.delete()
+    }
+}
