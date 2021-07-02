f1ca5ea7bd45b3385ff6ea7fbeabed0f2d8debd8
Criz <83665497+Criz-595@users.noreply.github.com>
2021-07-02 14:45:58 +0530

    Create basic rpc setup.js

---
+++ Discord Bot/basic rpc setup.js
@@ -0,0 +1,25 @@
+const RPC = require('discord-rpc');
+const { strict } = require('assert');
+const { warn } = require('console');
+const rpc = new RPC.Client({
+    transport: 'ipc'
+})
+
+rpc.on("ready", () => {
+    rpc.setActivity({
+        details: "",
+        state: "",
+        startTimestamp: new Date() ,
+        largeImageKey: "large",
+        largeImageText: "Elina bot",
+        smallImageKey: "small",
+        smallImageText: "DeezNuts",
+        buttons : [{label : "BOT NAME" , url : "URL"},{label : "Discord" , url : "SERVER LINK"}]
+    })
+
+    console.log("Successfully activated the RPC")
+})
+
+rpc.login({
+    clientId: 'YOUR USER ID'
+})
