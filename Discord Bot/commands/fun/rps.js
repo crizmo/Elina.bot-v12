9f28cb4392ffb8224d6ab4f5ef1d68d0439c70b9
Criz <83665497+Criz-595@users.noreply.github.com>
2021-07-03 10:59:58 +0530

    Update rps.js

--- Discord Bot/commands/fun/rps.js
+++ Discord Bot/commands/fun/rps.js
@@ -1,27 +1,76 @@
+const Discord = require("discord.js");
+
 module.exports = {
-    name: 'rps',
-    cooldown: 2,
-    permissions: ["SEND_MESSAGES"],
-    description: "rps command",
-    execute(client, message, args, Discord) {
-  
-      let question = args[0]
-  
-      if (!question) {
-        message.channel.send("Pls enter rock / paper / sissors")
-      }
-  
-      else {
-  
-        let responses = ["=rps rock", "=rps paper", "=rps scissors"]
-  
-        let response = Math.floor(Math.random() * responses.length)
+  name : 'rps',
+  permissions: ["SEND_MESSAGES"],
+  description: 'rock paper scissors command',
+  async execute (client, message, args, Discord) {
 
-        message.reply(responses[response])
+  let embed = new Discord.MessageEmbed()
+    .setTitle("Rock, Paper, Scissor")
+    .setColor("RANDOM")
+    .setDescription("*Choose your path.*")
+  m = await message.channel.send(embed);
+  await m.react("👊");
+  await m.react("✋"); 
+  await m.react("✌");
+  let choices = {};
+  await m.awaitReactions((reaction, user) => user.id === message.author.id, {max: 1, time: 60000, errors:['time','max']})
+    .then(collected => {
+      try{
+          succes = collected.get("👊");
+          if(succes){  
+              choices[message.author.id] = "r"
+          }
+      }catch(err){
 
-        if (!question == response) {
-          message.channel.send("I choose the same, Its a tie")   
-        }
       }
-    }
+      try{
+          succes = collected.get("✋");
+          if(succes){  
+              choices[message.author.id] = "p"
+          }
+      }catch(err){
+
+      }
+      try{
+          succes = collected.get("✌");
+          if(succes){  
+              choices[message.author.id] = "s"
+          }
+      }catch(err){
+
+      }
+  })
+  try{
+    await m.reactions.removeAll();
+  }catch(err){}
+  
+  pos = ["r","p","s"]
+  choices['ai'] = pos[Math.floor((Math.random() * pos.length))];
+  id = 'ai'
+  pos = {"r" : "👊","p" : "✋","s" : "✌"}
+  embed = new Discord.MessageEmbed()
+  .setDescription("You:" + pos[choices[message.author.id]] + "\n" + "Bot:" + pos[choices[id]])
+  switch("" + choices[message.author.id] + choices[id]){
+    case 'rs':
+    case 'pr':
+    case 'sp':
+        embed.setColor([0,255,0]).setTitle("This is the real **pro**");
+        m.edit(embed);
+        break;
+    case 'rp':
+    case 'ps':
+    case 'sr':
+        embed.setColor([255,0,0]).setTitle("**You** so weak bro, try again next time !");
+        m.edit(embed);
+        break;
+    case 'rr':
+    case 'pp':
+    case 'ss':
+        embed.setColor([200,200,0]).setTitle("**You** and **Bot** chose the same, so this match is a draw !");
+        m.edit(embed);
+        break;
   }
+}
+}
