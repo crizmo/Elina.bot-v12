281b1a5ed36beefe0b6ec15f600e3137470a6684
Criz <83665497+Criz-595@users.noreply.github.com>
2021-07-06 23:04:25 +0530

    Update command_handler.js

--- Discord Bot/events/handlers/command_handler.js
+++ Discord Bot/events/handlers/command_handler.js
@@ -10,55 +10,5 @@ module.exports = (client, Discord) =>{
         }else {
             continue;
         }
-    const command_files = fs.readdirSync(`./commands/fun/`).filter(file => file.endsWith('.js'))
-
-    for(const file of command_files){
-        const command = require(`../commands/fun/${file}`);
-        if(command.name){
-            client.commands.set(command.name, command);
-        }else { 
-            continue;
-        }
-    const command_files = fs.readdirSync(`./commands/moderation/`).filter(file => file.endsWith('.js'))
-
-    for(const file of command_files){
-        const command = require(`../commands/moderation/${file}`);
-        if(command.name){
-            client.commands.set(command.name, command);
-        }else {
-            continue;
-        }
-    const command_files = fs.readdirSync(`./commands/server/`).filter(file => file.endsWith('.js'))
-
-    for(const file of command_files){
-        const command = require(`../commands/server/${file}`);
-        if(command.name){
-            client.commands.set(command.name, command);
-        }else {
-            continue;
-        }
-    const command_files = fs.readdirSync(`./commands/affection/`).filter(file => file.endsWith('.js'))
-
-    for(const file of command_files){
-        const command = require(`../commands/affection/${file}`);
-        if(command.name){
-            client.commands.set(command.name, command);
-        }else {
-            continue;
-        }
-    const command_files = fs.readdirSync(`./commands/extras/`).filter(file => file.endsWith('.js'))
-
-    for(const file of command_files){
-        const command = require(`../commands/extras/${file}`);
-        if(command.name){
-            client.commands.set(command.name, command);
-        }else {
-            continue;
-        }
-    }
-    }
-    }
-    }
-    }   
     }
 }
