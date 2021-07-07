c27dd4d2ee89164e97343e41c2dfa7bc275a7890
Criz <83665497+Criz-595@users.noreply.github.com>
2021-07-07 10:01:19 +0530

    Add files via upload

---
+++ Discord Bot/commands/extras/chatbot.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'chatbot',
+    permissions: ["SEND_MESSAGES"],
+    description: "Embeds!",
+    execute(client, message, args, Discord){
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#00FFFF')
+        .setTitle('Elina chat bot')
+        .setURL('https://www.investopedia.com/terms/c/chatbot.asp')
+        .setDescription('Hi there')
+        .addFields(
+            {name: 'Chat bot setup', value: "Elina's chat bot feature is one of the most epik commands. To setup the chat bot, First make a channel named #chatbot , just a reminder remember to give the bot required perms to send message in the channel or it wont work. Then start by saying hey . There you are ready to go."},
+
+        )
+        .setThumbnail("https://cdn.discordapp.com/avatars/842397001954230303/a38b9242e08e6c5694c3edecd0afc64a.webp?size=1024")
+        .setFooter('Hope you enjoy the chat-bot command');
+
+        message.channel.send(newEmbed);
+    }
+}
